c9e5b5644a150aa6486cc367ee8b995b2b8ca93e
todendvv91 <todendvv91@gmail.com>
2023-10-02 23:37:27 +0900

    DOM 을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const login = () => {
+    const req = {
+        id : id.value,
+        password : password.value
+    };
+
+    // fetch();
+    console.log(req);
+};
+
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+
+// console.log(id);
+// console.log(password);
+// console.log(loginBtn);
