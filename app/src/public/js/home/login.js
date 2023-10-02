73487d496b9af7dffc44c5e239fa8e8a2825e8c0
todendvv91 <todendvv91@gmail.com>
2023-10-03 01:23:01 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,28 +1,29 @@
 "use strict";
 
 const login = () => {
-    const req = {
-        id : id.value,
-        password : password.value
-    };
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
 
-    fetch("/login", {
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify(req),
-    });
-    // console.log(req);
-    // console.log(JSON.stringify(req));
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+  // console.log(req);
+  // console.log(JSON.stringify(req));
 };
 
 const id = document.querySelector("#id"),
-    password = document.querySelector("#password"),
-    loginBtn = document.querySelector("button");
+  password = document.querySelector("#password"),
+  loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
-
 
 // console.log(id);
 // console.log(password);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,42 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const users = {
+  id: ["test", "정강우", "정팀장"],
+  password: ["1234", "1234", "1234"],
 };
 
-const login = (req, res) => {
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
     res.render("home/login");
+  },
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      password = req.body.password;
+
+    // console.log(id,password);
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다.",
+    });
+  },
 };
 
 module.exports = {
-    home,
-    login
-}
-
+  output,
+  process,
+};
