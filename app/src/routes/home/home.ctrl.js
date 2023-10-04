0c2e2bff188c9baf96594bbc64ff18747ffaf432
todendvv91 <todendvv91@gmail.com>
2023-10-04 10:10:14 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -15,27 +15,10 @@ const output = {
 };
 
 const process = {
-  login: (req, res) => {
+  login: async (req, res) => {
     const user = new User(req.body);
-    const response = user.login();
-    // console.log(response);
+    const response = await user.login();
     return res.json(response);
-    // const id = req.body.id,
-    //   password = req.body.password;
-    // const users = UserStorage.getUsers("id", "password");
-    // // console.log(UserStorage.getUsers("id", "password"));
-    // // console.log(id,password);
-    // const response = {};
-    // if (users.id.includes(id)) {
-    //   const idx = users.id.indexOf(id);
-    //   if (users.password[idx] === password) {
-    //     response.success = true;
-    //     return res.json(response);
-    //   }
-    // }
-    // response.success = false;
-    // response.msg = "로그인에 실패하였습니다.";
-    // return res.json(response);
   },
 
   register: (req, res) => {
